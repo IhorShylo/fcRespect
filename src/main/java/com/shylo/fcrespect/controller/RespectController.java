2fa19a8fbdd8ec46c1e986f2a24e8d7ef382071b
Ihor Shylo <i.shylo@whirl.sg>
2017-10-03 18:54:33 +0300

    Created dumb DataBase + added method that search Player by image name

--- src/main/java/com/shylo/fcrespect/controller/RespectController.java
+++ src/main/java/com/shylo/fcrespect/controller/RespectController.java
@@ -1,6 +1,8 @@
 package com.shylo.fcrespect.controller;
 
+import com.shylo.fcrespect.data.PlayersRepository;
 import com.shylo.fcrespect.model.Player;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import java.time.LocalDate;
 @Controller
 public class RespectController {
 
+    @Autowired
+    private PlayersRepository playersRepository;
+
     @RequestMapping(value = "/")
-    public String homePage(ModelMap modelMap){
-        Player player = new Player("Шило Игорь", LocalDate.of(1989, 12, 2), "Defender", "Tolik");
+    public String homePage(ModelMap modelMap) {
+        Player player = playersRepository.getPlayerByImgName("Den");
         modelMap.put("player", player);
         return "home";
     }
@@ -21,7 +26,7 @@ public class RespectController {
 
     @RequestMapping(value = "/players")
     @ResponseBody
-    public String listOfPlayers(){
+    public String listOfPlayers() {
         return "List of Players";
     }
 }
---
+++ src/main/java/com/shylo/fcrespect/data/PlayersRepository.java
@@ -0,0 +1,35 @@
+package com.shylo.fcrespect.data;
+
+import com.shylo.fcrespect.model.Player;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class PlayersRepository {
+    private static final List<Player> players = Arrays.asList(
+            new Player("Бабич Александр", LocalDate.of(1991, 2, 5), "Midfielder", "Babich"),
+            new Player("Брусиловский Дмитрий", LocalDate.of(1991, 5, 3), "Midfielder", "Brusilovskiy"),
+            new Player("Двирный Александр", LocalDate.of(1985, 1, 1), "Defender", "Dvirnuy"),
+            new Player("Д.Денис", LocalDate.of(1988, 3, 5), "Goalkeaper", "Den"),
+            new Player("Федя", LocalDate.of(1993, 8, 18), "Midfielder", "Fedya"),
+            new Player("Козырь Антон", LocalDate.of(1989, 6, 6), "Midfielder", "Kozyr"),
+            new Player("Лозница Богдан", LocalDate.of(1988, 10, 12), "Defender", "Loznytsa"),
+            new Player("Лысый Андрей", LocalDate.of(1988, 6, 7), "Midfielder", "Lysyi"),
+            new Player("Рускевич Максим", LocalDate.of(1985, 11, 26), "Forward", "Maks"),
+            new Player("Осадчий Сергей", LocalDate.of(1991, 7, 14), "Defender", "Osadchyi"),
+            new Player("Лубяный Павел", LocalDate.of(1990, 1, 20), "Midfielder", "Pasha"),
+            new Player("Шило Игорь", LocalDate.of(1989, 12, 2), "Defender", "Shylo"),
+            new Player("Сороченко Дмитрий", LocalDate.of(1988, 5, 11), "Midfielder", "Sorochenko"),
+            new Player("Кужелев Анатолий", LocalDate.of(1993, 4, 4), "Forward", "Tolik")
+    );
+
+    public Player getPlayerByImgName(String imgName) {
+        for (Player player : players) {
+            if (player.getImgName().equals(imgName)) return player;
+        }
+        return null;
+    }
+}
