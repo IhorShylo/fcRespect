efba609746bd6903146621d772b1a3987de24525
Ihor Shylo <i.shylo@whirl.sg>
2017-10-06 15:18:23 +0300

    Added i18

--- src/main/java/com/shylo/fcrespect/controller/RespectController.java
+++ src/main/java/com/shylo/fcrespect/controller/RespectController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -15,9 +15,11 @@ public class RespectController {
 
     @Autowired
     private PlayersRepository playersRepository;
+    private static List<String> localeCodes = Arrays.asList("en", "ru", "ua");
 
     @RequestMapping(value = "/")
     public String homePage(ModelMap modelMap) {
+        modelMap.put("localeCodes", localeCodes);
         return "home";
     }
 
@@ -25,7 +27,9 @@ public class RespectController {
     @RequestMapping(value = "/team")
     public String teamPage(ModelMap modelMap) {
         List<Player> allPlayers = playersRepository.getAllPlayers();
+        Player babich = playersRepository.getPlayerByImgName("Babich");
         modelMap.put("allPlayers", allPlayers);
+        modelMap.put("babich", babich);
         return "team";
     }
 }
